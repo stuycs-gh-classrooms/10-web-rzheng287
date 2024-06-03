2e701f12d791214119a117ba7035f6c72f2feb21
rzheng287 <144488534+rzheng287@users.noreply.github.com>
2024-06-03 15:02:35 -0400

    Add files via upload

---
+++ py/final02.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+print('Content-type: text/html\n')
+
+HTML_head = """
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+<link href="final02.css" rel="stylesheet">
+<meta charset="utf-8">
+<title>Hello</title>
+</head>
+"""
+
+HTML_link = '''
+<a href="temp_url">temp_text</a>'''
+    
+
+HTML_foot = """
+</body>
+</html>
+"""
+
+HTML_list ='''
+<li>temp_list</li>
+'''
+
+def add_link(s,url,text):
+    new = ''
+    link = s.replace('temp_url',url)
+    add_text = link.replace('temp_text',text)
+    new += add_text
+    return new
+
+
+
+
+
